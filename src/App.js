f1e8a561636797971c43a9e5c90492d905196af4
MITESH PATEL <mitforu@gmail.com>
2020-02-08 13:14:59 -0600

    Adds routing to the project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import Game from "./game/Game";
+import Home from "./home/Home";
+import { BrowserRouter, Switch, Route, Link } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-        <Game/>
-    </div>
+        <div className="App">
+            <BrowserRouter>
+                <div>
+                           <Link to="/home">Home</Link>{' '}
+                          <Link to="/game">Game</Link>{' '}
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route exact path="/home" component={Home} />
+                        <Route exact path="/game" component={Game} />
+                    </Switch>
+                </div>
+            </BrowserRouter>
+        </div>
   );
 }
 
